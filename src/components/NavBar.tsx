29eb2d05762eb5a5c5c2b5f72cfff6050273d72b
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-10-10 07:44:28 +0200

    working with the homepage

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div>
+      <div>
+        <div>Ana Cascante</div>
+        <div>
+          <a
+            href="#home>Home</a>
+            <a href="
+            #home
+          >
+            About me
+          </a>
+          <a
+            href="#home>Projects</a>
+            <a href="
+            #home
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
