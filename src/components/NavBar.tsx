bb6eb5fa9d4c09098938393fafae59ec8f5bb3fa
Ana Cascante <91411170+AnaCascante@users.noreply.github.com>
2024-10-11 06:22:38 +0200

    removing not usefull href

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,22 +5,7 @@ const NavBar = () => {
     <div>
       <div>
         <div>Ana Cascante</div>
-        <div>
-          <a
-            href="#home>Home</a>
-            <a href="
-            #home
-          >
-            About me
-          </a>
-          <a
-            href="#home>Projects</a>
-            <a href="
-            #home
-          >
-            Contact
-          </a>
-        </div>
+        <div></div>
       </div>
     </div>
   );
