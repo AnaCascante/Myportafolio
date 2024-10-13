7b1458e17c28ed109c381a7dc393dd6a89f96539
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-10-13 20:05:25 +0200

    adds projects to portafolio

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -32,7 +32,7 @@ const NavBar = () => {
             <SiAboutdotme />
           </Link>
           <Link
-            to="technologies"
+            to="projects"
             smooth={true}
             duration={500}
             className="hover:scale- inline transform rounded-full bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% px-4 py-2 text-2xl text-yellow-50 transition-transform duration-300 hover:scale-150"
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,31 +1,45 @@
 import { FaGithub } from 'react-icons/fa';
 import { SiRender } from 'react-icons/si';
+import Eazyshop from '../images/Eazy-shop.png';
+import Exam2 from '../images/Exam2.png';
+import norway from '../images/norway.png';
 
 const projects = [
   {
     id: 1,
-    title: 'Project 1',
-    description: 'This is a description of project 1.',
-    image: 'https://via.placeholder.com/150',
-    github: 'https://www.example.com',
-    link: 'https://www.example.com',
+    title: 'Holidaze',
+    description:
+      'Holidaze is an accommodation booking application designed for both customers and venue managers. Customers can explore and book various venues, while venue managers can list, manage, and view bookings for their properties. The app features user authentication, dynamic search functionality, and a responsive UI for seamless booking experiences.',
+    image: Exam2,
+    github: 'https://github.com/AnaCascante/holidaze',
+    link: 'https://holidaze-inzc.onrender.com',
   },
   {
     id: 2,
-    title: 'Project 2',
-    description: 'This is a description of project 2.',
-    image: 'https://via.placeholder.com/150',
+    title: 'Eazy-shop',
+    description:
+      'E-Commerce Project. This project is a fully responsive e-commerce application built using JavaScript frameworks. I utilized React for building dynamic and interactive components, Tailwind CSS for utility-first responsive styling, and additional CSS for fine-tuned design elements. The application allows users to browse products, add items to the cart, and proceed to checkout, providing a smooth and efficient shopping experience.',
+    image: Eazyshop,
+    github:
+      'https://github.com/AnaCascante/eazy-shop-1.3/blob/master/README.md',
+    link: 'https://eazy-shop1-3.netlify.app/',
+  },
+  {
+    id: 3,
+    title: 'Auction House',
+    description: 'Stay tuned! This project will be available soon🤓',
+    image: norway,
     github: 'https://www.example.com',
     link: 'https://www.example.com',
   },
-  {
+  /*{
     id: 3,
     title: 'Project 3',
     description: 'This is a description of project 3.',
     image: 'https://via.placeholder.com/150',
     github: 'https://www.example.com',
     link: 'https://www.example.com',
-  },
+  },*/
 ];
 
 const Projects = () => {
