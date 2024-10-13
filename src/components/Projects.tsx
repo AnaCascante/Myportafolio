418f715c3225b045f65bcc8b96144752d2de7a65
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-10-13 19:24:50 +0200

    adds cards for the projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,24 +3,27 @@ import { SiRender } from 'react-icons/si';
 
 const projects = [
   {
+    id: 1,
     title: 'Project 1',
     description: 'This is a description of project 1.',
     image: 'https://via.placeholder.com/150',
-    Github: 'https://www.example.com',
+    github: 'https://www.example.com',
     link: 'https://www.example.com',
   },
   {
+    id: 2,
     title: 'Project 2',
     description: 'This is a description of project 2.',
     image: 'https://via.placeholder.com/150',
-    Github: 'https://www.example.com',
+    github: 'https://www.example.com',
     link: 'https://www.example.com',
   },
   {
+    id: 3,
     title: 'Project 3',
     description: 'This is a description of project 3.',
     image: 'https://via.placeholder.com/150',
-    Github: 'https://www.example.com',
+    github: 'https://www.example.com',
     link: 'https://www.example.com',
   },
 ];
@@ -29,16 +32,23 @@ const Projects = () => {
   return (
     <div className="bg-black py-20 text-yellow-50" id="projects">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
-        <h2 className="mb-12 text-center text-4xl font-bold">My Projects</h2>
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+        <h2 className="mb-16 text-center text-4xl font-bold">My Projects</h2>
+        <div className="mb-16 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
           {projects.map((project) => (
-            <div>
-              <img src={project.image} alt={project.title} />
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
+            <div
+              key={project.id}
+              className="transform rounded-lg bg-gray-800 p-6 transition-transform duration-300 hover:scale-105 hover:shadow-lg"
+            >
+              <img
+                src={project.image}
+                alt={project.title}
+                className="mb-4 h-48 w-full rounded-lg object-cover"
+              />
+              <h3 className="mb-2 text-2xl font-bold">{project.title}</h3>
+              <p className="mb-4 text-gray-400">{project.description}</p>
               <div className="flex justify-center space-x-10">
                 <a
-                  href="https://www.linkedin.com/in/ana-cascante-de-quadras-4301521a/"
+                  href={project.link}
                   target="_blank"
                   rel="noreferrer"
                   className="inline-flex transform items-center justify-center rounded-full bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 p-4 text-yellow-50 transition-transform duration-300 hover:scale-150"
@@ -47,7 +57,7 @@ const Projects = () => {
                 </a>
 
                 <a
-                  href="https://github.com/AnaCascante"
+                  href={project.github}
                   target="_blank"
                   rel="noreferrer"
                   className="inline-flex transform items-center justify-center rounded-full bg-gradient-to-r from-indigo-500 via-sky-500 to-emerald-500 p-4 text-yellow-50 transition-transform duration-300 hover:scale-150"
